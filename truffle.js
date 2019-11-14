6ea24fc3bbee23d4153e5c2ee49c8d4b15dd6204
sam bacha <32783916+mer1dian@users.noreply.github.com>
2019-11-14 12:47:43 -0800

    tuffle

--- truffle.js
+++ truffle.js
@@ -3,10 +3,17 @@ require('babel-register')
 
 module.exports = {
   networks: {
+     freight: {
+      host: '54.82.25.227',
+      port: 8546,
+      network_id: '211' 
+    }
+    
     ganache: {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*' // Match any network id
-    }
+  }
+    
   }
 }
